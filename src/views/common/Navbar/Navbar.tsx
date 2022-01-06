969c2cef5871afa3e6f8c0d0f491132255a696f9
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-01-06 20:38:00 +0300

    Navbar.tsx updated

--- src/views/common/Navbar/Navbar.tsx
+++ src/views/common/Navbar/Navbar.tsx
@@ -9,25 +9,27 @@ export const Navbar: React.FC = () => {
 
   return (
     <nav>
-      <div className="fixed w-full h-8 bg-blue-400 text-gray-200 flex flex-row justify-between items-center">
-        <div className="brand-logo font-roboto font-bold px-2">ERSİNSHOP</div>
-        <ul className="hidden menu-list lg:flex lg:flex-row text-xs font-poppins font-bold">
-          <li className="menu-list-item">
+      <div className="fixed w-full h-8 bg-blue-400 text-gray-200 flex flex-row justify-between items-center md:justify-center">
+        <div className="brand-logo font-roboto font-bold px-2  md:hidden">
+          ERSİNSHOP
+        </div>
+        <ul className="hidden menu-list md:flex md:flex-row text-xs font-poppins font-bold md:justify-around">
+          <li className="menu-list-item px-2 mx-2 ">
             <a href="/home">Home</a>
           </li>
-          <li className="menu-list-item px-2">
+          <li className="menu-list-item px-2 mx-2">
             <a href="/about">About</a>
           </li>
-          <li className="menu-list-item px-2">
+          <li className="menu-list-item px-2 mx-2">
             <a href="/contact">Contact</a>
           </li>
-          <li className="menu-list-item px-2">
+          <li className="menu-list-item px-2 mx-2">
             <a href="/profile">Profile</a>
           </li>
         </ul>
         <button
           onClick={() => toggleSideBarOpen()}
-          className="lg:hidden menu-button"
+          className="md:hidden menu-button"
         >
           {isSideBarOpen ? <X size={24} /> : <List size={24} />}
         </button>
